a69a645fad5d0ebc8ee962e093c73bff943e7400
anamoyee <106310501+anamoyee@users.noreply.github.com>
2026-03-05 17:55:47 +0100

    add publish.sh

---
+++ publish.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+
+# build, then publish via uv
+
+# assume PYPI token is stored in GNU password-store
+# at /pypi_tokens/{TOKEN_NAME}
+
+TOKEN_NAME="__main__"
+
+if [[ TOKEN_NAME == "" ]]; then
+	echo "Provide a TOKEN_NAME in a form of a (GNU password-store) path" >&2
+	exit 1
+fi
+
+rm ./dist -rf # remove builds of old versions, dont upload more and more every time
+
+uv build
+
+UV_PUBLISH_USERNAME="__token__" UV_PUBLISH_PASSWORD="$(/usr/bin/env pass "pypi_tokens/${TOKEN_NAME}")" uv publish
+
